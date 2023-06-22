6d4c069c023c94d96791f4070e057f266179f799
Codeskar <zielasoon@gmail.com>
2023-06-22 20:49:44 +0200

    url handler

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/constants/constants.dart';
 import 'package:news_app/models/news_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsCard extends StatelessWidget {
   final Article article;
@@ -9,40 +10,49 @@ class NewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
-      height: 130,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: CachedNetworkImage(
-              imageUrl: article.urlToImage == null
-                  ? ApiUrls.imageNotFound
-                  : article.urlToImage.toString(),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-              width: 130,
-              height: 130,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Text(
-              article.title.toString(),
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () => _launchUrl(Uri.parse(article.url.toString())),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+        height: 130,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: CachedNetworkImage(
+                imageUrl: article.urlToImage == null
+                    ? ApiUrls.imageNotFound
+                    : article.urlToImage.toString(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                width: 130,
+                height: 130,
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-        ],
+            const SizedBox(width: 10),
+            Expanded(
+              child: Text(
+                article.title.toString(),
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
